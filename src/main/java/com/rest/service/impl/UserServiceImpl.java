a2da6aca5d30f4b818811162734053c6304c756b
HamzaELKhatib <hamza.elkhatib@protonmail.com>
2022-04-20 00:15:11 +0000

    Random userId

--- src/main/java/com/rest/service/impl/UserServiceImpl.java
+++ src/main/java/com/rest/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.rest.service.impl;
 import com.rest.UserRepository;
 import com.rest.io.entity.UserEntity;
 import com.rest.service.UserService;
+import com.rest.shared.Utils;
 import com.rest.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        UserEntity userInDb = userRepository.findByEmail(user.getEmail());
+
+        if(userInDb != null) throw new RuntimeException("User already exists");
 
         UserEntity userEntity = new UserEntity();
 
@@ -24,9 +32,9 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(user, userEntity);
 
         // Populating the required fields that aren't put in by the user
-        userEntity.setUserId("userId");
+        userEntity.setUserId(utils.generateUserId(10));
 
-        userEntity.setEncryptedPassword("password");
+        userEntity.setEncryptedPassword("hardcodedPassword");
 
         // saving info from userEntity to the database using the repo
         UserEntity storedUserDetails = userRepository.save(userEntity);
@@ -40,6 +48,7 @@ public class UserServiceImpl implements UserService {
 
         // Returning the dto
         return returnValue;
+
     }
 
 
