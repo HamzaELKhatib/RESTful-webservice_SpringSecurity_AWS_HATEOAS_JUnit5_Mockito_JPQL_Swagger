b44dcd0b67d98ccae49443906d3c50d4ac44fa2a
HamzaELKhatib <hamza.elkhatib@protonmail.com>
2022-08-10 22:17:08 +0100

    Generating a war and deploying on apache tomcat

--- src/main/java/com/rest/RestApplication.java
+++ src/main/java/com/rest/RestApplication.java
@@ -3,11 +3,19 @@ package com.rest;
 import com.rest.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
+
+	// this method is used to help generate a war file
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
